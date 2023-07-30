2489eb19ed7aa6e72f62220a82d3f219985f4e8c
Jet <thazledine@hotmail.co.uk>
2023-07-30 17:14:33 +0100

    changed data shape

--- models/plant.js
+++ models/plant.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 
 const plantSchema = new mongoose.Schema({
-  common_name: { type: String, required: true },
+  name: { type: String, required: true },
   id: { type: String, required: false },
-  cycle: { type: String, required: false },
-  watering: {type: String, required:false},
-  sunlight: {type: String, required: false}
+  waterLast: {type: String, required:false},
+  position: {type: String, required: false}
 });
 
 export default mongoose.model('Plant', plantSchema);

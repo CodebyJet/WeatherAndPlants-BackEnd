e5d2284c485256f7c1ad1ec42d461f34e28536bc
Jet <thazledine@hotmail.co.uk>
2023-08-01 15:20:08 +0100

    plant model

--- models/plant.js
+++ models/plant.js
@@ -3,8 +3,8 @@ import mongoose from 'mongoose';
 const plantSchema = new mongoose.Schema({
   name: { type: String, required: true },
   id: { type: String, required: false },
-  waterLast: {type: String, required:false},
-  position: {type: String, required: false}
+  waterLast: { type: String, required: false },
+  position: { type: String, required: false }
 });
 
 export default mongoose.model('Plant', plantSchema);
